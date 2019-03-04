3114af750f1cbf87604cc08d8da0312361348e76
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-04 23:27:50 +0000

    Add possible parameter to tick() in countdown(), to prepare to send activity data to this function. Will this work? Whooooo knowwwwsss.....

--- timer.js
+++ timer.js
@@ -2,21 +2,19 @@ const gameTime = 3 // min
 const activityTime = 10 // sec
 
 function countdown(minutes) {
-    var seconds = 60;
+    var seconds = 60
     var mins = minutes
-    function tick() {
-        var counter = document.getElementById("counter");
-        var current_minutes = mins-1
-        seconds--;
-        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-        if( seconds > 0 ) {
-            setTimeout(tick, 1000);
-        } else {
-            if(mins > 1){
-                countdown(mins-1);
-            }
+    function tick(skipTime) {
+        var counter = document.getElementById("counter")
+        var current_minutes = mins - 1
+        skipTime ? seconds = seconds - skipTime : seconds--
+        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds)
+        if (seconds > 0 ) {
+            setTimeout(tick, 1000)
+        } else if (mins > 1) {
+            countdown(mins - 1)
         }
     }
-    tick();
+    tick()
 }
-countdown(gameTime);
+countdown(gameTime)
