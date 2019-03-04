877a2377566473572606393d9449e4ea4664de36
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-04 23:39:31 +0000

    Try a new countdown timer, one that allows for pauses. Borrowed from https://stackoverflow.com/questions/38539000/javascript-countdown-with-pause-resume

--- timer.js
+++ timer.js
@@ -1,20 +1,56 @@
-const gameTime = 3 // min
-const activityTime = 10 // sec
+var timersCount = 0;
+var pause = false; //is timer paused
 
-function countdown(minutes) {
-    var seconds = 60
-    var mins = minutes
-    function tick(skipTime) {
-        var counter = document.getElementById("counter")
-        var current_minutes = mins - 1
-        skipTime ? seconds = seconds - skipTime : seconds--
-        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds)
-        if (seconds > 0 ) {
-            setTimeout(tick, 1000)
-        } else if (mins > 1) {
-            countdown(mins - 1)
-        }
+countTimers();
+
+function countTimers() {
+  timersCount++;
+
+  var count = 26;
+  var counter = setInterval(timer, 1000);
+
+  function timer() {
+    if (!pause) { //do something if not paused
+      count = count - 1;
+      if (count < 0) {
+        clearInterval(counter);
+        setTimeout(countTimers, 5000); //start count from 26 again
+        return;
+      }
+
+      document.getElementById("timer").innerHTML = count;
     }
-    tick()
+  }
+
+  document.getElementById("countTimers").innerHTML = timersCount;
 }
-countdown(gameTime)
+
+document.getElementById('pause').addEventListener('click', function () {
+    pause = true;
+});
+
+document.getElementById('resume').addEventListener('click', function () {
+    pause = false;
+});
+
+// const gameTime = 3 // min
+// const activityTime = 10 // sec
+
+// function countdown(minutes) {
+//     var seconds = 60
+//     var mins = minutes
+//     function tick(skipTime) {
+//         var counter = document.getElementById("counter")
+//         var current_minutes = mins - 1
+//         seconds--
+//         // skipTime ? seconds = seconds - skipTime : seconds--
+//         counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds)
+//         if (seconds > 0 ) {
+//             setTimeout(tick, 1000)
+//         } else if (mins > 1) {
+//             countdown(mins - 1)
+//         }
+//     }
+//     tick()
+// }
+// countdown(gameTime)
