e7355d1e3e2bff1ff09c1d55394d6cb38f906ae3
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-04 23:07:50 +0000

    Remove comments and begin transforming basic count to accept functionality the game requires.

--- timer.js
+++ timer.js
@@ -1,16 +1,10 @@
-// https://gist.github.com/adhithyan15/4350689
-/********************************************************************************************************************
-Countdown.js is a simple script to add a countdown timer
-for your website. Currently it can only do full minutes
-and partial minutes aren't supported. This script is a fork of http://jsfiddle.net/HRrYG/ with some
-added extensions. Since the original code that I forked was released under Creative Commons by SA license,
-I have to release this code under the same license. You can view a live demo of this code at http://jsfiddle.net/JmrQE/2/.
-********************************************************************************************************************/
+const gameTime = 3 // min
+const activityTime = 10 // sec
+
 function countdown(minutes) {
     var seconds = 60;
     var mins = minutes
     function tick() {
-        //This script expects an element with an ID = "counter". You can change that to what ever you want.
         var counter = document.getElementById("counter");
         var current_minutes = mins-1
         seconds--;
@@ -25,5 +19,4 @@ function countdown(minutes) {
     }
     tick();
 }
-//You can use this script with a call to onclick, onblur or any other attribute you would like to use.
-countdown(3);//where n is the number of minutes required.
+countdown(gameTime);
