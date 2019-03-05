7b80eb53b8f8d71fae508603b87c7f9aa71bd67c
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-05 02:32:44 +0000

    Add additional health metrics to player.

--- public/player.js
+++ public/player.js
@@ -5,7 +5,17 @@ const player = Crafty.e('Player, 2D, DOM, Color, Fourway, Collision, Keyboard')
         y: 10,
         w: 40,
         h: 40,
-        energy: 7
+        energy: 6,
+        stress: 6,
+        mindPlatter: {
+            timeIn: 6,
+            downTime: 6,
+            focusTime: 6,
+            playTime: 6,
+            connectingTime: 6,
+            sleepTime: 6,
+            physicalTime: 6
+        }
     })
     .color('red')
     .fourway(200)
