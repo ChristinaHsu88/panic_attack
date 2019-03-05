06e25803cd09d6f0affea0c1cdc1a99cb1026629
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-05 00:05:17 +0000

    Remove commented out code.

--- timer.js
+++ timer.js
@@ -26,29 +26,3 @@ document.onkeydown = function (e) {
         pause ? pause = false : pause = true
     }
 }
-
-// document.getElementById('resume').addEventListener('click', function () {
-//     pause = false;
-// });
-
-// const gameTime = 3 // min
-// const activityTime = 10 // sec
-
-// function countdown(minutes) {
-//     var seconds = 60
-//     var mins = minutes
-//     function tick(skipTime) {
-//         var counter = document.getElementById("counter")
-//         var current_minutes = mins - 1
-//         seconds--
-//         // skipTime ? seconds = seconds - skipTime : seconds--
-//         counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds)
-//         if (seconds > 0 ) {
-//             setTimeout(tick, 1000)
-//         } else if (mins > 1) {
-//             countdown(mins - 1)
-//         }
-//     }
-//     tick()
-// }
-// countdown(gameTime)
