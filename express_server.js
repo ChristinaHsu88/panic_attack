453ed3c93ba453fb012ac74f6a36106c2c80fe49
ChristinaHsu88 <christina.hsu.ca@gmail.com>
2019-03-05 14:27:03 -0800

    update the pseudo db in express.js

--- express_server.js
+++ express_server.js
@@ -9,15 +9,20 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 
 /* pseudo DB */
-let platter = {
+let userInfo = {
     "user1": {
-        timeIn: 7,
-        sleep: 7,
-        downTime: 7,
-        play: 7,
-        physical: 7,
-        connecting: 7,
-        focus: 7
+        days_play : {
+            days: 0
+        },
+        platter: {
+            timeIn: 7,
+            sleep: 7,
+            downTime: 7,
+            play: 7,
+            physical: 7,
+            connecting: 7,
+            focus: 7
+        }
     }
 }
 
@@ -28,13 +33,13 @@ app.get('/', function(req, res) {
 
 /* send data to client side */
 app.get('/data', function (req, res) {
-    let data = platter
+    let data = userInfo
     res.json(data)
 })
 
-/* add data to the pseudo DB now */
+/* add data to the pseudo DB now with a temp user name 'user2' */
 app.post('/', function(req, res) {
-    platter['user2'] = req.body
+    userInfo['user2'] = req.body
 })
 
 app.listen(PORT, () => {
