2de219d8e8315d106c52bde458a69a17b25b33f7
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-06 23:15:37 +0000

    Revise player entity attr to include hard-coded global playerMetrics variable.

--- public/player.js
+++ public/player.js
@@ -5,17 +5,7 @@ const player = Crafty.e('Player, 2D, DOM, Color, Fourway, Collision, Keyboard')
         y: 10,
         w: 40,
         h: 40,
-        metrics: {
-            energy: 6,
-            stress: 6,
-            timeIn: 6,
-            downTime: 6,
-            focusTime: 6,
-            playTime: 6,
-            connectingTime: 6,
-            sleepTime: 6,
-            physicalTime: 6
-        }
+        metrics: playerMetrics
     })
     .color('red')
     .fourway(200)
