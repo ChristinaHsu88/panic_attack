196afbe6e58af4e7e954f1e2296cc14fe3b89c6a
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-07 21:49:49 +0000

    Replace hardcoded sprite with variable that can/will be changed in scoring.

--- public/stress-bar.js
+++ public/stress-bar.js
@@ -3,6 +3,8 @@ Crafty.sprite(70, 'assets/green.png', { greenStress:[0, 0] }) // stress 3-5
 Crafty.sprite(70, 'assets/orange.png', { orangeStress:[0, 0] }) // stress 6-8
 Crafty.sprite(70, 'assets/red.png', { redStress:[0, 0] }) // stress 9-10
 
+let stressLevel = 'blueStress'
+
 Crafty.e('2D, DOM, StressBar')
   .attr({x: 370, y: 280})
-  .addComponent('blueStress')
+  .addComponent(stressLevel)
