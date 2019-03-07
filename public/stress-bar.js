207f489c9f84456e013febc756e16a3d09ccef2b
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-07 21:46:42 +0000

    Name stress bar entity and use addComponent method to specify sprite.

--- public/stress-bar.js
+++ public/stress-bar.js
@@ -1,7 +1,8 @@
-// Crafty.sprite(70, 'assets/blue.png', { bar:[0, 0] }) // stress 0-2
-Crafty.sprite(70, 'assets/green.png', { bar1:[0, 0] }) // stress 0-5
-Crafty.sprite(70, 'assets/orange.png', { bar2:[0, 0] }) // stress 6-8
-Crafty.sprite(70, 'assets/red.png', { bar3:[0, 0] }) // stress 9-10
+Crafty.sprite(70, 'assets/blue.png', { blueStress:[0, 0] }) // stress 0-2
+Crafty.sprite(70, 'assets/green.png', { greenStress:[0, 0] }) // stress 3-5
+Crafty.sprite(70, 'assets/orange.png', { orangeStress:[0, 0] }) // stress 6-8
+Crafty.sprite(70, 'assets/red.png', { redStress:[0, 0] }) // stress 9-10
 
-Crafty.e('2D, DOM, bar')
+Crafty.e('2D, DOM, StressBar')
   .attr({x: 370, y: 280})
+  .addComponent('blueStress')
