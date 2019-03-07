9da9f5de17ea01cbb2d2db25f83ebac6240fe316
samanthamarienock <sammarienock@gmail.com>
2019-03-07 09:14:48 -0800

    renders the loading page map!

--- public/game.js
+++ public/game.js
@@ -7,3 +7,12 @@ Crafty.init(500, 350, document.getElementById('game'));
 // timer
 
 // lets set up the scenes?
+Crafty.scene('loading', function() {
+  Crafty.background('white url(assets/loading.png) no-repeat center center');
+  Crafty.e('2D, DOM, Text')
+    .attr({ x: 350, y: 200 })
+    .text('Welcome!')
+    .textFont({ size: '40px', weight: 'bold' });
+});
+
+Crafty.scene('loading');
