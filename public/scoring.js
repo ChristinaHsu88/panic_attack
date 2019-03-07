3cb804fe90a69d7710b230e79a04717a8776d25a
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-07 00:38:41 +0000

    Add/rearrange comments on scoring.js

--- public/scoring.js
+++ public/scoring.js
@@ -1,11 +1,10 @@
 
-// universal variable that persists throughout gameplay until server is restarted
-// to be sent/saved to DB at the end of the game
+// universal variable that persists throughout gameplay until browser is refreshed
 const playerMetrics = {
-        daysPlayed : 0,
+        daysPlayed : 0, // increment up at end of day
         metrics: {
-            stress: 0,
-            energy: 0,
+            stress: 0, // affected directly actions (up and down, sometimes with same action); indirectly by all
+            energy: 0, // affected directly by time and eating; indirectly by playTime, sleepTime, physicalTime
             timeIn: 0,
             downTime: 0,
             focusTime: 0,
@@ -14,16 +13,14 @@ const playerMetrics = {
             sleepTime: 0,
             physicalTime: 0
         }
-    }
+    } // save to DB at end of game
 
-startingScore(playerMetrics)
+startingScore(playerMetrics) // calculate player metrics at start of game
+setTimeout(timeScoreChanger, 30000, playerMetrics.metrics)
 
 function startingScore(metrics){
   let baseScore;
-  if (metrics.daysPlayed === 0) {
-  // need to write algorithms for future days, keeping in mind the other metrics will also affect
-  // when game ends, the daysPlayed will need to be updated
-  // this way, if a user returns to play a game and pulls their playerMetrics from the db, the algorithm I need to write will determine their stress and energy levels
+  if (metrics.daysPlayed === 0) { // TODO else if algorithms
     baseScore = 6
   }
   for (let metric in metrics.metrics) {
@@ -42,13 +39,19 @@ function timeScoreChanger(metrics){
   }
   console.log('Time Score Changer: \n', metrics)
   if (document.getElementById("timer").innerHTML > 0) {
-    setTimeout(timeScoreChanger, 30000, metrics) // run every 30 while game in play
+    setTimeout(timeScoreChanger, 30000, metrics) // run every 30s while game in play
   }
   return metrics
 }
 
-// console.log('Day One Score: \n', startingScore(playerMetrics.metrics))
-setTimeout(timeScoreChanger, 30000, playerMetrics.metrics)
+
+// methods needed:
+  // calculateStressAndEnergy
+    // to be run after every metrics change
+    // if any platter metric falls below 1, stress goes up every 10s
+    // if any platter metric rises, stress should drop
+  // disable available actions if energy too low
+  // only some actions should affect stress directly?
 
 /* Metrics:
     - primary:
