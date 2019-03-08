9faab1541f6fb111b4d14b2f2fab8b2fbc40b1a2
samanthamarienock <sammarienock@gmail.com>
2019-03-08 15:20:10 -0800

    has walkable character

--- public/player.js
+++ public/player.js
@@ -1,63 +1,70 @@
 // define and generate player
-Crafty.sprite(56, 'assets/dude.png', {
-  WalkAway: [0, 0]
-});
 
-const player = Crafty.e(
-  'Player, 2D, DOM, Fourway, Collision, Keyboard, WalkAway'
-)
-  .attr({
-    x: 10,
-    y: 10
-    // w: 40,
-    // h: 40
-  })
-  // .color('red')
-  .fourway(200)
-  .checkHits('Item')
-  .bind('HitOn', function(hitItem) {
-    itemPopUp(hitItem);
-  })
-  .bind('HitOff', function() {
-    Crafty('ItemPopUp').destroy();
-  })
-  .bind('KeyDown', function(e) {
-    // to check score during development
-    if (e.key == Crafty.keys.SHIFT) {
-      console.log('Player stats (primary):', playerMetrics.primaryMetrics);
-      console.log('Player stats (platter):', playerMetrics.platter);
-    }
-  });
+function makePlayer() {
+  Crafty.e(
+    'Player, 2D, DOM, SpriteAnimation, Fourway, Collision, Keyboard, walk1'
+  )
+    .attr({
+      x: 10,
+      y: 10
+      // w: 40,
+      // h: 40
+    })
+    .reel('WalkAway', 5000, [
+      'walk1',
+      'walk2',
+      'walk3',
+      'walk4',
+      'walk5',
+      'walk6',
+      'walk7',
+      'walk8',
+      'walk9'
+    ])
+    .animate('WalkAway', -1)
+    // .color('red')
+    .fourway(200)
+    .checkHits('Item')
+    .bind('HitOn', function(hitItem) {
+      itemPopUp(hitItem);
+    })
+    .bind('HitOff', function() {
+      Crafty('ItemPopUp').destroy();
+    })
+    .bind('KeyDown', function(e) {
+      // to check score during development
+      if (e.key == Crafty.keys.SHIFT) {
+        console.log('Player stats (primary):', playerMetrics.primaryMetrics);
+        console.log('Player stats (platter):', playerMetrics.platter);
+      }
+    });
 
-// sprite animation
-// .reel('animation name', length of animation, x axis on sprite map, y axis on sprite map, frame count, ant of frames in sprite sheet row)
+  // sprite animation
+  // .reel('animation name', length of animation, x axis on sprite map, y axis on sprite map, frame count, ant of frames in sprite sheet row)
 
-// .pauseAnimation() --- pauses the currently playing animation
-// .animate(animation name, number loop count)
+  // .pauseAnimation() --- pauses the currently playing animation
+  // .animate(animation name, number loop count)
 
-//Crafty.sprite(32, 'image url path', {
-//PlayerSprite: [0,0]
+  //Crafty.sprite(32, 'image url path', {
+  //PlayerSprite: [0,0]
 
-//});
+  //});
 
-//Crafty.e("2D, DOM, SpriteAnimation, PlayerSprite")
-//.reel('PlayerRunning', 20, 0, 0, 3)
-//.animate('playerRunning, -1);
-//create crafty component
-// Crafty.c('Penny', {
-//init: function() {
-//set up the animation
-// .animate('WalkAway', 100, 0, 0, 8)
-// .animate('WalkLeft', 100, 0, 1, 8)
-// .animate('WalkTowards', 100, 0, 2, 8)
-// .animate('WalkRight', 100, 0, 3, 8)
+  //create crafty component
+  // Crafty.c('Penny', {
+  // init: function() {
+  // .animate('WalkAway', 100, 0, 0, 8)
+  // .animate('WalkLeft', 100, 0, 1, 8)
+  // .animate('WalkTowards', 100, 0, 2, 8)
+  // .animate('WalkRight', 100, 0, 3, 8);
 
-// give it direction conditionals
+  // // give it direction conditionals
 
-//.bind('DirectionChange',
-// function(direction) .......
+  // // .bind('DirectionChange',
+  // // function(direction) .......
 
-// )};
+  // // )};
 
-//});
-//});
+  // // });
+  // });
+}
--- public/scene-bedroom.js
+++ public/scene-bedroom.js
@@ -2,12 +2,18 @@
 Crafty.scene('bedroom', function() {
   Crafty.background('white url(assets/bedroom2.png) no-repeat center center');
   // TODO - change cat sprite for player
-  //   Crafty.sprite(50, 'assets/dude.png', {
-  //     player: [0, 0]
-  //   });
-  const player = Crafty.e(
-    'Player, 2D, DOM, Fourway, Collision, Keyboard, WalkAway'
-  );
+  Crafty.sprite(32, 'assets/dude.png', {
+    walk1: [3, 0],
+    walk2: [4, 0],
+    walk3: [5, 0]
+    // walk4: [3, 3],
+    // walk5: [4, 3],
+    // walk6: [5, 3],
+    // walk7: [6, 3],
+    // walk8: [7, 3],
+    // walk9: [8, 3]
+  });
+
   // generate all entities in this scene
   makePlayer();
   generateRoomItems();
