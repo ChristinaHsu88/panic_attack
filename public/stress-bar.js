e7fa440310993002e4621e9a9a0c1b84e9550b6b
Shmoo Ritchie <44276124+shmootidy@users.noreply.github.com>
2019-03-07 17:02:20 -0800

    Update stress-bar.js
    Update size of sprites to align with new assets.

--- public/stress-bar.js
+++ public/stress-bar.js
@@ -1,7 +1,7 @@
-Crafty.sprite(70, 'assets/blue.png', { blueStress:[0, 0] }) // stress 0-2
-Crafty.sprite(70, 'assets/green.png', { greenStress:[0, 0] }) // stress 3-5
-Crafty.sprite(70, 'assets/orange.png', { orangeStress:[0, 0] }) // stress 6-8
-Crafty.sprite(70, 'assets/red.png', { redStress:[0, 0] }) // stress 9-10
+Crafty.sprite(300, 'assets/blue.png', { blueStress:[0, 0] }) // stress 0-2
+Crafty.sprite(300, 'assets/green.png', { greenStress:[0, 0] }) // stress 3-5
+Crafty.sprite(300, 'assets/orange.png', { orangeStress:[0, 0] }) // stress 6-8
+Crafty.sprite(300, 'assets/red.png', { redStress:[0, 0] }) // stress 9-10
 
 let stressColor = 'blueStress' // must hold value for entity creation, could be anything
 
