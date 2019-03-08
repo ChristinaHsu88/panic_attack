bfe6a9502d6071e2e51dee63604e4a80df46554c
samanthamarienock <sammarienock@gmail.com>
2019-03-07 16:05:54 -0800

    creates some screen transition -- still not perfect

--- public/game.js
+++ public/game.js
@@ -6,23 +6,50 @@ Crafty.init(600, 350, document.getElementById('game'));
 // player
 // timer
 
-// lets set up the scenes?
+// Scene loader
+function load_scene(scene, duration) {
+  Crafty.e('2D, Canvas, Tween, Color')
+    .attr({ alpha: 0.0, x: 0, y: 0, w: 800, h: 600 })
+    .color('#000000')
+    .tween({ alpha: 1.0 }, duration)
+    .bind('TweenEnd', function() {
+      Crafty.scene(scene);
+      Crafty.e('2D, Canvas, Tween, Color')
+        .attr({ alpha: 1.0, x: 0, y: 0, w: 800, h: 600 })
+        .color('#000000')
+        .tween({ alpha: 0.0 }, duration);
+    });
+}
+
+Crafty.defineScene('test', function() {
+  Crafty.background('white');
+  load_scene('loading', 3000);
+});
+
+load_scene('test', 3000);
+
 Crafty.scene('loading', function() {
-  //this sets the 'loading' scene
   Crafty.background('white url(assets/loading.png) no-repeat center center');
-  Crafty.e('2D, DOM, Text')
+  Crafty.e('2D, DOM, Text, Mouse')
     .attr({ x: 350, y: 200 })
     .text('Welcome!')
-    .textFont({ size: '40px', weight: 'bold' });
+    .textFont({ size: '40px', weight: 'bold' })
+    .bind('Click', function(MouseEvent) {
+      Crafty.enterScene('bedroom');
+    });
+
+  // bedroom scene
+  Crafty.scene('bedroom', function() {
+    Crafty.background('white url(assets/bedroom2.png) no-repeat center center');
+  });
 });
+// Crafty.scene('loading');
+// lets set up the scenes?
+// Crafty.scene('loading', function() {
+//this sets the 'loading' scene
+// });
 // this calls the scene and renders it
-Crafty.scene('loading');
 
 //how do i transition between scenes in craftyjs?
 
-// bedroom scene
-Crafty.scene('bedroom', function() {
-  Crafty.background('white url(assets/bedroom2.png) no-repeat center center');
-});
-
-Crafty.scene('bedroom');
+// Crafty.scene('bedroom');
