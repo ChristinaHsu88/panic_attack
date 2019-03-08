741854a32ee19a1e5d6f956172b93b806a00d6b9
ChristinaHsu88 <christina.hsu.ca@gmail.com>
2019-03-08 14:55:51 -0800

    add username input feature. DB can grab the user input

--- public/scene-welcome.js
+++ public/scene-welcome.js
@@ -4,18 +4,41 @@
             // if new name, create entry in DB
             // if name exists, pull entry from DB
         // if game has rolled over from previous round, user should not have to type in player name but simply hit enter to start next day
+
+const alphabet = 
+{A:'A',B:'B',C:'C',D:'D',E:'E',F:'F',G:'G',H:'H',I:'I',J:'J',K:'K',L:'L',M:'M',
+N:'N',O:'O',P:'P',Q:'Q',R:'R',S:'S',T:'T',U:'U',V:'V',W:'W',X:'X',Y:'Y',Z:'Z'}
+
 Crafty.scene('welcome', function() {
     Crafty.background('white url(assets/loading.png) no-repeat center center')
+    /* greetings */
     Crafty.e('2D, DOM, Text, Mouse')
-    .attr({ x: 350, y: 200 })
-    .text('hit enter')
+    .attr({ x: 230, y: 180 })
+    .text('Hello! ')
+    .textFont({ size: '40px', weight: 'bold' })
+
+    /* store username */
+    let username = ''
+    Crafty.e('2D, DOM, Text, Mouse')
+    .attr({ x: 230, y: 220 })
+    .text(username)
     .textFont({ size: '40px', weight: 'bold' })
     .bind('KeyDown', function(e) {
-      if (e.key == Crafty.keys.ENTER) {
-        Crafty.enterScene('bedroom')
-        startingScore(playerMetrics) // calculate player metrics at start of game
+        if (e.key == Crafty.keys.ENTER) {
+            Crafty.enterScene('bedroom')
+            timer()
+            startingScore(playerMetrics)          // calculate player metrics at start of game
+            playerMetrics.name = username         /* store username to playerMetrics */
+            console.log('inside the keydown', playerMetrics)
         }
-    })
+        for (let letter in alphabet) {            /* loop through alphabet to find letter that user enters and store in username */
+            if (e.key == Crafty.keys[letter]) {
+                this.text(alphabet[letter])
+                username += this.text(alphabet[letter])._text
+                this.text(username)
+            }
+        }
+    }) 
 })
 
 function loadWelcome(scene, duration) {
@@ -33,3 +56,4 @@ function loadWelcome(scene, duration) {
 }
 
 loadWelcome('welcome', 0);
+
