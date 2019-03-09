36cf0d46f4399a3dc582c18d36b85a1acfac1f1d
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-09 21:45:10 +0000

    Pseudocode prompts.js

--- public/prompts.js
+++ public/prompts.js
@@ -1,3 +1,21 @@
-function promptEat(){}
-function promptNap(){}
-function promptWorldEvents(){}
+// DRY helper
+function allPrompts(options){
+  // pause game
+  // pop up with options
+  // skip time forward if YES
+}
+
+function promptEat(){
+  // allPrompts(options)
+  // increase energy if YES
+}
+
+function promptNap(){
+  // allPrompts(options)
+  // increase sleep if YES
+}
+
+function promptWorldEvents(){
+  // allPrompts(options)
+  // generate appropriate in-game response based on event options
+}
