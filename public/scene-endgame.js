7012db76d3947e1b4fbd38ac0c1e72c8444a3793
ChristinaHsu88 <christina.hsu.ca@gmail.com>
2019-03-09 13:43:03 -0800

    add customized message on the first end game page

--- public/scene-endgame.js
+++ public/scene-endgame.js
@@ -1,52 +1,25 @@
-// two versions of the ending:
-  // panic and time out
-  // the only difference between the two is the message that appears and the quantity of cats on the bed
 Crafty.scene('endgame', function() {
   Crafty.background('white url(assets/end.png) no-repeat center center')  /* image for testing purpose */
 
-  /* GAME OVER && message */
+  /* GAME OVER && message depending on the stress level */
   Crafty.e('2D, DOM, Canvas, Text, Mouse')
-  .attr({ x: 190, y: 70 })
-  .text('Game over! ')
-  .textFont({ size: '50px', weight: 'bold'})
+  .attr({ x: 20, y: 70 })
+  .text(function() {
+    console.log('Hello')
+    console.log(playerMetrics.primaryMetrics.stress)
+    if (playerMetrics.primaryMetrics.stress > 14) {
+      return 'You gotta learn how to chill.'
+    } else {
+      return 'Time for some good sleep'
+    }
+  })
   .textColor('white')
+  .textFont({ size: '50px', weight: 'bold'})
   .bind('KeyDown', function(e) {
     if (e.key == Crafty.keys.ENTER) {
-      console.log('made it here')
-      console.log(playerMetrics.primaryMetrics.stress)
-      if (playerMetrics.primaryMetrics.stress > 14) {
-        console.log('this should load the cat page')
-      } 
-      if (playerMetrics.primaryMetrics.stress < 14) {
-        console.log('this should load the sleep page only')
-      }
-        /* sample code */
-        // playerMetrics.name = username
-        // if (username === '') {                  /* alert user to enter a name */
-        //     alert('you have to enter a name!')
-        // } else {
-        // Crafty.enterScene('bedroom')            /* scene will only load when the user has entered a name */
-        // timer()
-        // checkUser(username) // checks DB for existing user; sets playerMetrics to saved DB
-        // startingScore(playerMetrics)
-        // }
+      Crafty.enterScene('nightCats')
     }
-    // TODO /* all done */
-        // let user backspace or clear name before enter
-        // tell user how to log in (i.e., press enter)
-        // do not allow empty username
-    for (let letter in alphabet) {
-        if (e.key == Crafty.keys[letter]) {
-            username += this.text(alphabet[letter])._text
-            this.text(username)
-        } 
-    }
-    if (e.key == Crafty.keys.BACKSPACE) {       /* allow user to edit their name */
-        username = username.slice(0, - 1)
-        this.text(username)
-    }
-})
-
+  })
 })
 
 function loadEndgame(scene, duration) {
---
+++ public/scene-nightCats.js
@@ -0,0 +1,5 @@
+/* post endScene - bedroom with no cats */
+Crafty.scene('nightCats', function() {
+    Crafty.background('white url(assets/sleepWCats.jpg) no-repeat center right')
+    /* into the final scene */
+})
