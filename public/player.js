d8919de6662fcd856bc526fdc17ea58ff13fff35
samanthamarienock <sammarienock@gmail.com>
2019-03-09 17:07:35 -0800

    failed attempt at creating a wall

--- public/player.js
+++ public/player.js
@@ -1,24 +1,25 @@
 // define and generate player
-  // player entity will be destroyed and regenerated in each scene
-function makePlayer (argument) {
-  Crafty.e('Player, 2D, DOM, Fourway, Collision, Keyboard, player')
-      .attr({
-        x: 10,
-        y: 10,
-        w: 40,
-        h: 40,
-      })
-      .fourway(200)
-      .checkHits('Item')
-      .bind('HitOn', function(hitItem) {
-        itemPopUp(hitItem)
-      })
-      .bind('HitOff', function() {
-        Crafty('ItemPopUp').destroy()
-      })
-      .bind('KeyDown', function(e) { // to check score during development
-        if (e.key == Crafty.keys.SHIFT) {
-          console.log('Player stats: \n', playerMetrics)
-        }
-  })
+// player entity will be destroyed and regenerated in each scene
+function makePlayer(argument) {
+  Crafty.e('Player, 2D, DOM, Fourway, Keyboard, player, Collision')
+    .attr({
+      x: 10,
+      y: 10,
+      w: 40,
+      h: 40
+    })
+    .fourway(200)
+    .checkHits('Item')
+    .bind('HitOn', function(hitItem) {
+      itemPopUp(hitItem);
+    })
+    .bind('HitOff', function() {
+      Crafty('ItemPopUp').destroy();
+    })
+    .bind('KeyDown', function(e) {
+      // to check score during development
+      if (e.key == Crafty.keys.SHIFT) {
+        console.log('Player stats: \n', playerMetrics);
+      }
+    });
 }
