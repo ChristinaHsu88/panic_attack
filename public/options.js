cd553285de07cc943e31d3d4379cf890a5ef87e8
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-11 21:20:10 +0000

    Refactor. Move options handling to its own file.

--- public/options.js
+++ public/options.js
@@ -94,36 +94,7 @@ function makePopUp (hitItem) {
             } else if (e.key == Crafty.keys.ENTER && this.selectOption.canSelect) {
                 // find option
                 const selectedOption = Crafty(this.selectOption.optionObj['0'])
-                // find option's effects
-                const scoreEffect = selectedOption.scoreEffect
-                // update score based on option
-                if (scoreEffect) {
-                    for (effect in scoreEffect.platter) {
-                        playerMetrics.platter[effect] += scoreEffect.platter[effect]
-                    }
-                    for (effect in scoreEffect.primaryMetrics) {
-                        playerMetrics.primaryMetrics[effect] += scoreEffect.primaryMetrics[effect]
-                    }
-                    loseTime()
-                    calculateStress(playerMetrics)
-                }
-                // find world event's effects
-                const playerMove = selectedOption.playerMove
-                if (playerMove) {
-                    // TODO - send player to selected scene
-                    console.log(`Player moved ${playerMove}! (You just can't tell yet.)`)
-                    Crafty.enterScene(playerMove)
-                    loseTime()
-                }
-                // find new skill
-                const newSkill = selectedOption.newSkill
-                // display call info
-                if (newSkill) {
-                    setTimeout(takeCall, 200, newSkill)
-                    // moved from gainNewSkill in prompts under therapist call
-                    playerMetrics.previousDays.newSkill = true
-                    document.getElementById('new-skill').innerText = '"SHIFT" - body check' // TODO - prettify
-                }
+                handleOption(selectedOption)
                 Crafty('player').unfreeze()
                 Crafty('Option, OptionsBox, Selector').destroy()
             }
