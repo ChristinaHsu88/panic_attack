bb1f05f200b8016ccebeeb66caa8ad98ade3771e
samanthamarienock <sammarienock@gmail.com>
2019-03-11 11:37:25 -0700

    sets up sprite rendering for animation

--- public/player.js
+++ public/player.js
@@ -1,44 +1,72 @@
 Crafty.c('Player', {
   init: function() {
-    this.addComponent('2D, DOM, Fourway, Collision, Keyboard, player')
-    this.w = 40
-    this.h = 40
+    this.addComponent('2D, DOM, Fourway, Collision, Keyboard, player');
+    this.w = 40;
+    this.h = 40;
   },
   place: function(x, y) {
-    this.x = x
-    this.y = y
-    return this
+    this.x = x;
+    this.y = y;
+    return this;
   }
-})
+});
 // define and generate player
-  // player entity will be destroyed and regenerated in each scene
-function makePlayer (x, y) {
-  Crafty.e('Player, 2D, DOM, Fourway, Collision, Keyboard, player')
+// player entity will be destroyed and regenerated in each scene
+function makePlayer(x, y) {
+  Crafty.e('Player, 2D, DOM, Fourway, Collision, Keyboard, PlayerToward')
     .place(x, y)
     .fourway(200)
     .checkHits('Item')
     .bind('HitOn', function(hitItem) {
-      itemPopUp(hitItem)
-      renderNewScene(hitItem)
+      itemPopUp(hitItem);
+      renderNewScene(hitItem);
     })
     .bind('HitOff', function() {
-      Crafty('ItemPopUp').destroy()
+      Crafty('ItemPopUp').destroy();
     })
-    .bind('KeyDown', function(e) { // to check score during development
+    .bind('KeyDown', function(e) {
+      // to check score during development
       if (e.key === Crafty.keys.SHIFT) {
-        console.log('Player stats: \n', playerMetrics)
+        console.log('Player stats: \n', playerMetrics);
       }
     })
+    .bind('Move', function(evt) {
+      var hitDatas, hitData;
+      if ((hitDatas = this.hit('Solid'))) {
+        hitData = hitDatas[0];
+        if (hitData.type === 'SAT') {
+          this.x -= hitData.overlap * hitData.nx;
+          this.y -= hitData.overlap * hitData.ny;
+        } else {
+          this.x = evt._x;
+          this.y = evt._y;
+        }
+      }
+    });
+  //CHARACTER ANIMATION
+  //LEFT_ARROW: 37,
+  // UP_ARROW: 38,
+  // RIGHT_ARROW: 39,
+  // DOWN_ARROW: 40
 }
 
-/* new scene */
+// /* new scene */
 function renderNewScene(hitItem) {
-  const location = hitItem['0'].obj.location
-  if (location === "livingroom") {
-    Crafty.enterScene(location)
+  const location = hitItem['0'].obj.location;
+  console.log(hitItem);
+  if (location === 'livingroom') {
+    Crafty.enterScene('livingroom');
   } else if (location === 'outside') {
-    Crafty.enterScene(location)
-  } else if (location === "bedroom") {
-    Crafty.enterScene(location)
+    Crafty.enterScene('outside');
+  } else if (location === 'bedroom') {
+    Crafty.enterScene('bedroom');
+    const location = hitItem['0'].obj.location;
+    if (location === 'livingroom') {
+      Crafty.enterScene(location);
+    } else if (location === 'outside') {
+      Crafty.enterScene(location);
+    } else if (location === 'bedroom') {
+      Crafty.enterScene(location);
+    }
   }
 }
--- public/scene-bedroom.js
+++ public/scene-bedroom.js
@@ -1,19 +1,73 @@
 // scene 2 - bedroom
 Crafty.scene('bedroom', function() {
-    Crafty.background('#57342E url(assets/bedroom2.png) no-repeat center right');
-    // TODO - change cat sprite for player
-    Crafty.sprite(32, 'assets/cat.png', { player: [0, 1] })
-    Crafty.sprite(100, 'assets/phone_2.png', { phone: [0, 0] })
-    Crafty.sprite(100, 'assets/weed.png', { weed: [0, 0]})
-    Crafty.sprite(100, 'assets/laundry.png', {laundry: [0]})
-    // generate all entities in this scene
-    if (gameTime > 178) {
-        makePlayer(340, 50)
-    } else {
-        makePlayer(400, 250)
-    }
-    generateRoomItems()
-    createStressBar()    
-    // likely have to Crafty.enterScene('endGame') here, but wrap in a function so that it's triggered by endGame()
-    // it seems like .enterScene needs to be called within the scene that is active, therefore all scenes will need endGame
-})
+  Crafty.background('#57342E url(assets/bedroom2.png) no-repeat center right');
+  // TODO - change cat sprite for player
+  Crafty.sprite(32, 'assets/dude.png', {
+    PlayerToward: [3, 0],
+    PlayerLeft: [3, 1],
+    PlayerRight: [3, 2],
+    PlayerAway: [3, 3]
+  });
+  // generate all entities in this scene
+  //WALLS:
+  Crafty.e('2D, DOM, Color, Solid, WallLeft, Collision,').attr({
+    x: 87,
+    y: 0,
+    h: 350,
+    w: 5
+  });
+  Crafty.e('2D, DOM, Solid, WallTop, Collision').attr({
+    x: 87,
+    y: 5,
+    h: 5,
+    w: 350
+  });
+  Crafty.e('2D, DOM, Solid, WallRight, Collision').attr({
+    x: 690,
+    y: 0,
+    h: 350,
+    w: 5
+  });
+
+  Crafty.e('2D, DOM, Solid, WallBottomRight, Collision').attr({
+    x: 70,
+    y: 250,
+    h: 5,
+    w: 300
+  });
+
+  Crafty.e('2D, DOM, WallBottomLeft, Solid, Collision').attr({
+    x: 450,
+    y: 250,
+    h: 5,
+    w: 300
+  });
+
+  //WALLS
+  console.log('how much game time left', gameTime);
+  if (gameTime > 178) {
+    makePlayer(340, 50);
+  } else {
+    makePlayer(400, 250);
+  }
+  createWalls();
+  generateRoomItems();
+  createStressBar();
+  // likely have to Crafty.enterScene('endGame') here, but wrap in a function so that it's triggered by endGame()
+  // it seems like .enterScene needs to be called within the scene that is active, therefore all scenes will need endGame
+});
+Crafty.background('#57342E url(assets/bedroom2.png) no-repeat center right');
+//     // TODO - change cat sprite for playe
+Crafty.sprite(100, 'assets/phone_2.png', { phone: [0, 0] });
+Crafty.sprite(100, 'assets/weed.png', { weed: [0, 0] });
+Crafty.sprite(100, 'assets/laundry.png', { laundry: [0] });
+// generate all entities in this scene
+if (gameTime > 178) {
+  makePlayer(340, 50);
+} else {
+  makePlayer(400, 250);
+}
+generateRoomItems();
+createStressBar();
+// likely have to Crafty.enterScene('endGame') here, but wrap in a function so that it's triggered by endGame()
+// it seems like .enterScene needs to be called within the scene that is active, therefore all scenes will need endGame
