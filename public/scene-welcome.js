c8112390c2ccbe017d823a7131b79735045c19ad
samanthamarienock <sammarienock@gmail.com>
2019-03-12 16:09:02 -0700

    creates new fonts

--- public/scene-welcome.js
+++ public/scene-welcome.js
@@ -36,40 +36,44 @@ Crafty.scene('welcome', function() {
   Crafty.background('white url(assets/loading.png) no-repeat center center');
   /* greetings */
   Crafty.e('2D, DOM, Canvas, Text, Mouse')
-    .attr({ x: 130, y: 200 })
-    .text('Hello! Please enter your name :')
-    .textFont({ size: '30px', weight: 'bold', type: 'italic' })
-    .textColor('black');
+    .attr({ x: 45, y: 250 })
+    .text('enter your name:')
+    .textFont({ size: '25px', weight: 'bold' })
+    .textColor('black')
+    .css({ 'font-family': 'VT323' })
+    .css({ 'font-family': 'monospace' });
 
   /* grab username & start game */
   let username = '';
   Crafty.e('2D, DOM, Text, Mouse')
-    .attr({ x: 270, y: 240 })
+    .attr({ x: 300, y: 240 })
     .text(username)
-    .textFont({ size: '35px', weight: 'bold' })
+    .textFont({ size: '30px', weight: 'bold' })
     .textColor('white')
+    .css({ 'font-family': 'VT323' })
+    .css({ 'font-family': 'monospace' })
     .bind('KeyDown', function(e) {
       if (e.key == Crafty.keys.ENTER) {
         playerMetrics.name = username;
         if (username === '') {
-          alert('you have to enter a name!')
+          alert('you have to enter a name!');
         } else {
-          Crafty.enterScene('bedroom') // destroys this scene
-          currentLocation = 'bedroom'
-          console.log('tracking initial location ', currentLocation)
+          Crafty.enterScene('bedroom'); // destroys this scene
+          currentLocation = 'bedroom';
+          console.log('tracking initial location ', currentLocation);
           timer();
-          checkUser(username) // checks DB for exising user; startingScore called within
+          checkUser(username); // checks DB for exising user; startingScore called within
         }
       }
       for (let letter in alphabet) {
         if (e.key == Crafty.keys[letter]) {
           username += this.text(alphabet[letter])._text;
-          this.text(username)
+          this.text(username);
         }
       }
       if (e.key == Crafty.keys.BACKSPACE) {
-        username = username.slice(0, -1)
-        this.text(username)
+        username = username.slice(0, -1);
+        this.text(username);
       }
     });
 });
@@ -84,8 +88,8 @@ function loadWelcome(scene, duration) {
       Crafty.e('2D, DOM, Tween, Color')
         .attr({ alpha: 1.0, x: 0, y: 0, w: 800, h: 600 })
         .color('#000000')
-        .tween({ alpha: 0.0 }, duration)
-    })
+        .tween({ alpha: 0.0 }, duration);
+    });
 }
 
 loadWelcome('welcome', 0);
@@ -100,8 +104,8 @@ function checkUser(username) {
       for (let user in db) {
         if (db[user].gameData.name === username) {
           // console.log('User Found!', db[user].gameData)
-          playerMetrics = db[user].gameData
-          return
+          playerMetrics = db[user].gameData;
+          return;
         }
       }
     })
@@ -109,6 +113,6 @@ function checkUser(username) {
       startingScore(playerMetrics);
     })
     .catch(function(error) {
-      console.log(error)
-    })
+      console.log(error);
+    });
 }
