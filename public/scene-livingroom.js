5ea482b27fd3eb8f1a6034e07460a51cb9d59a8f
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-13 03:24:45 +0000

    Remove load bearing wall. (jkjkjk)

--- public/scene-livingroom.js
+++ public/scene-livingroom.js
@@ -9,14 +9,6 @@ Crafty.scene('livingroom', function() {
   Crafty.sprite(32, 'assets/dog.png', { dog: [0, 0] });
   Crafty.sprite(32, 'assets/treadmill.png', { treadmill: [0, 0] });
   Crafty.sprite(32, 'assets/roommate.png', { roommate: [0, 0] });
-  /* wall */
-  /* keep this wall here for now as a reference for the side door */
-  Crafty.e(
-    '2D, DOM, Color, Solid, WallLeft, Collision,'
-  )
-    .attr({ x: 120, y: 0, h: 350, w: 5 })
-    .css({ visibility: 'hidden' })
-    .color('black');
 
   Crafty.e('2D, DOM, Color, Solid, WallTop, Collision')
     .attr({ x: 87, y: 56, h: 5, w: 600 })
--- public/scene-livingroom2.js
+++ public/scene-livingroom2.js
@@ -5,17 +5,11 @@ Crafty.scene('livingroom2', function() {
   Crafty.background(
     '#57342E url(assets/livingroom_update.png) no-repeat center right'
   );
+
   Crafty.sprite(32, 'assets/cat.png', { player: [0, 1] });
   Crafty.sprite(32, 'assets/dog.png', { dog: [0, 0] });
   Crafty.sprite(32, 'assets/treadmill.png', { treadmill: [0, 0] });
-  Crafty.sprite(32, 'assets/roommate.png', { roommate: [0, 0] }); // TODO - is this still here?
-  /* wall */
-  /* keep this wall here for now as a reference for the side door */ Crafty.e(
-    '2D, DOM, Color, Solid, WallLeft, Collision,'
-  )
-    .attr({ x: 120, y: 0, h: 350, w: 5 })
-    .css({ visibility: 'hidden' })
-    .color('black');
+  Crafty.sprite(32, 'assets/roommate.png', { roommate: [0, 0] });
 
   Crafty.e('2D, DOM, Color, Solid, WallTop, Collision')
     .attr({ x: 87, y: 56, h: 5, w: 600 })
