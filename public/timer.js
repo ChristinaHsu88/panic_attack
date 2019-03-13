b8ad3dcff97b13bc45fc63e0244e6d0fc6b3c2d5
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-13 08:08:43 +0000

    Change worldTimes array.

--- public/timer.js
+++ public/timer.js
@@ -8,7 +8,7 @@ function timer() {
   setInterval(tickTock, 250)
   document.getElementById("timer").innerHTML = gameTime
 
-  const worldTimes = [160, 130, 100, 70, 40, 10].sort(() => 0.5 - Math.random()).slice(0, 2) // returns two nums randomly from array
+  const worldTimes = [160, 130, 100, 70, 40, 15].sort(() => 0.5 - Math.random()).slice(0, 2) // returns two nums randomly from array
   const scoreChangeTimes = [150, 120, 90, 60, 30]
   const eatPromptTimes = [145, 95, 45]
 
