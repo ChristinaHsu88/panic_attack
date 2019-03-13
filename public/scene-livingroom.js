5fe8a848b43a99c1ca6efaa236e0f5cd2f95cfe6
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-13 03:28:50 +0000

    Return those walls. They actually WERE load bearing. Remove their color and styling instead.
    (Crafty doesn't seem to like css invisible.)

--- public/scene-livingroom.js
+++ public/scene-livingroom.js
@@ -9,6 +9,13 @@ Crafty.scene('livingroom', function() {
   Crafty.sprite(32, 'assets/dog.png', { dog: [0, 0] });
   Crafty.sprite(32, 'assets/treadmill.png', { treadmill: [0, 0] });
   Crafty.sprite(32, 'assets/roommate.png', { roommate: [0, 0] });
+  /* wall */
+  /* keep this wall here for now as a reference for the side door */
+  Crafty.e(
+    '2D, DOM, Color, Solid, WallLeft, Collision,'
+  )
+    .attr({ x: 120, y: 0, h: 350, w: 5 })
+    .color();
 
   Crafty.e('2D, DOM, Color, Solid, WallTop, Collision')
     .attr({ x: 87, y: 56, h: 5, w: 600 })
--- public/scene-livingroom2.js
+++ public/scene-livingroom2.js
@@ -5,11 +5,16 @@ Crafty.scene('livingroom2', function() {
   Crafty.background(
     '#57342E url(assets/livingroom_update.png) no-repeat center right'
   );
-
   Crafty.sprite(32, 'assets/cat.png', { player: [0, 1] });
   Crafty.sprite(32, 'assets/dog.png', { dog: [0, 0] });
   Crafty.sprite(32, 'assets/treadmill.png', { treadmill: [0, 0] });
-  Crafty.sprite(32, 'assets/roommate.png', { roommate: [0, 0] });
+  Crafty.sprite(32, 'assets/roommate.png', { roommate: [0, 0] }); // TODO - is this still here?
+  /* wall */
+  /* keep this wall here for now as a reference for the side door */ Crafty.e(
+    '2D, DOM, Color, Solid, WallLeft, Collision,'
+  )
+    .attr({ x: 120, y: 0, h: 350, w: 5 })
+    .color();
 
   Crafty.e('2D, DOM, Color, Solid, WallTop, Collision')
     .attr({ x: 87, y: 56, h: 5, w: 600 })
