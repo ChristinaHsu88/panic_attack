12e7c58369bd4178168c8046e42fad2ff8efce35
Shmoo Ritchie <shmooritchie@gmail.com>
2019-03-13 00:38:31 +0000

    Move loseTime call to specific conditionals in handle-options. Otherwise EVERYTHING loses time, including 'GO BACK'.

--- public/handle-options.js
+++ public/handle-options.js
@@ -13,11 +13,13 @@ function handleOption(selectedOption) {
             playerMetrics.primaryMetrics[effect] += scoreEffect.primaryMetrics[effect]
         }
         calculateStress(playerMetrics)
+        loseTime()
     }
     // move player
     if (playerMove) {
         Crafty.enterScene(playerMove)
         currentLocation = playerMove // update player's location (req'd for world event prompts - prompts.js)
+        loseTime()
     }
     // display call info
     if (newSkill) {
@@ -26,5 +28,4 @@ function handleOption(selectedOption) {
         playerMetrics.previousDays.newSkill = true // enable newSkill
         document.getElementById('new-skill').innerText = '"SHIFT" - body check' // TODO - prettify
     }
-    loseTime()
 }
